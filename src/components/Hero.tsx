b38c7f13ded420b97e24e281da53e4216c571908
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-03 22:12:45 +0530

    feat: create hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,15 @@
 import React from "react";
+import "./Hero.css";
 
 function Hero() {
   return (
-    <div>
-      <h1>Groceries delivered in 10 minutes</h1>
-      <p>Fresh vegetables, fruits, dairy and more..</p>
-    </div>
+    <>
+      <div className="hero">
+        <h1>Groceries delivered in 10 minutes</h1>
+        <p>Fresh vegetables, fruits and dairy delivered to your doorstep</p>
+        <button>Shop Now</button>
+      </div>
+    </>
   );
 }
 
