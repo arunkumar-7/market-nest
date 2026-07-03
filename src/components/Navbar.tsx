3872d01e96dce4438b77c1ad5cca441d4d6f5ba0
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-03 21:46:38 +0530

    create navbar ui for marketnest

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,32 @@
 import React from "react";
 import logo from "../assets/images/logo-marketnest.png";
 import "./Navbar.css";
+import { FaShoppingCart } from "react-icons/fa";
 function Navbar() {
   return (
     <>
-      <div className="navbar">
-        <div className="logo-section">
+      <nav className="navbar">
+        <div className="nav-left">
           <img src={logo} alt="logo" className="logo-img" />
           <h2>MarketNest</h2>
-          <p>Delivering Fast</p>
         </div>
-      </div>
+        <div className="nav-location">
+          <p className="deliver-text">Deliver to</p>
+          <h4>Warasiguda, Hyderabad ▼</h4>
+        </div>
+        <div className="nav-search">
+          <input
+            type="text"
+            placeholder="Search vegetables, fruits, dairy..."
+          />
+        </div>
+        <div className="nav-buttons">
+          <button>Login</button>
+          <button className="cart-btn">
+            <FaShoppingCart /> Cart
+          </button>
+        </div>
+      </nav>
     </>
   );
 }
