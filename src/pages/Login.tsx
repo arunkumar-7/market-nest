3bd86c3e9406fcd156923164a5346b806b3bf757
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-07 20:36:24 +0530

    feat : create login page UI

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,35 @@
-import React from "react";
+import "./Login.css";
+import { Link } from "react-router-dom";
 
 function Login() {
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <div className="login-container">
+      <div className="login-card">
+        <h1>Welcome Back</h1>
+        <p>Login to continue shopping</p>
+
+        <form>
+          <input
+            type="email"
+            placeholder="Enter Email"
+          />
+
+          <input
+            type="password"
+            placeholder="Enter Password"
+          />
+
+          <button type="submit">
+            Login
+          </button>
+        </form>
+
+        <p>
+          Don't have an account?{" "}
+          <Link to="/register">Register</Link>
+        </p>
+      </div>
+    </div>
   );
 }
 
