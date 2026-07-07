114a761c272612382e44f6ee9080e22df8f737ee
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-07 20:59:51 +0530

    feat: integrate react-hook-form into register page

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,75 +1,55 @@
+import { useForm } from "react-hook-form";
 import "./Register.css";
-import { Link } from "react-router-dom";
+import { data, Link } from "react-router-dom";
 
 function Register() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
+
   return (
     <div className="register-container">
       <div className="register-card">
         <h1>Create Account</h1>
         <p>Register to continue shopping</p>
 
-        <form>
-          <input
-            type="text"
-            placeholder="Enter Name"
-          />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input type="text" placeholder="Enter Name" />
 
-          <input
-            type="email"
-            placeholder="Enter Email"
-          />
+          <input type="email" placeholder="Enter Email" />
 
-          <input
-            type="password"
-            placeholder="Enter Password"
-          />
+          <input type="password" placeholder="Enter Password" />
 
-          <input
-            type="tel"
-            placeholder="Enter Phone Number"
-          />
+          <input type="tel" placeholder="Enter Phone Number" />
 
           <div className="gender-group">
             <label>Gender</label>
 
             <div className="gender-options">
               <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="Male"
-                />
+                <input type="radio" name="gender" value="Male" />
                 Male
               </label>
 
               <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="Female"
-                />
+                <input type="radio" name="gender" value="Female" />
                 Female
               </label>
 
               <label>
-                <input
-                  type="radio"
-                  name="gender"
-                  value="Other"
-                />
+                <input type="radio" name="gender" value="Other" />
                 Other
               </label>
             </div>
           </div>
 
-          <button type="submit">
-            Create Account
-          </button>
+          <button type="submit">Create Account</button>
         </form>
 
         <p className="login-link">
-          Already have an account?{" "}
-          <Link to="/login">Login</Link>
+          Already have an account? <Link to="/login">Login</Link>
         </p>
       </div>
     </div>
