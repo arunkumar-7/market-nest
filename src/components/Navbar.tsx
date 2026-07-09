f34bb778e1b529e176dcacae94e0bba5e3fbf9dd
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-09 20:58:45 +0530

    chore: backup before ui redesign

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import logo from "../assets/images/logo-marketnest.png";
+import { useState } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
 import { useCart } from "../hooks/useCart";
