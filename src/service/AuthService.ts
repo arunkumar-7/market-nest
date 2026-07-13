2895c0120c738f23135de525a15d6be451e59c00
arunkumar-7 <arunkumarbathini4@gmail.com>
2026-07-13 16:22:18 +0530

    feat(auth): integrate frontend with Spring Boot authentication API

--- src/service/AuthService.ts
+++ src/service/AuthService.ts
@@ -1,51 +1,20 @@
 import type { LoginRequest } from "../interfaces/LoginRequest";
 import type { RegisterRequest } from "../interfaces/RegisterRequest";
+import { loginUser, registerUser } from "../api/AuthApi";
 
 export const register = async (data: RegisterRequest) => {
-  const users: RegisterRequest[] = JSON.parse(
-    localStorage.getItem("users") || "[]",
-  );
-
-  const userExists = users.some((user) => user.email === data.email);
-
-  if (userExists) {
-    throw new Error("Email already registered");
-  }
-
-  users.push(data);
-
-  localStorage.setItem("users", JSON.stringify(users));
-
-  return {
-    data: {
-      success: true,
-      message: "Customer Registered Successfully",
-    },
-  };
+  return await registerUser(data);
 };
 
 export const login = async (data: LoginRequest) => {
-  const users: RegisterRequest[] = JSON.parse(
-    localStorage.getItem("users") || "[]",
+  const response = await loginUser(data);
+
+  localStorage.setItem(
+    "currentUser",
+    JSON.stringify(response.data.data),
   );
 
-  const user = users.find(
-    (u) => u.email === data.email && u.password === data.password,
-  );
-
-  if (!user) {
-    throw new Error("Invalid Email or Password");
-  }
-
-  localStorage.setItem("currentUser", JSON.stringify(user));
-
-  return {
-    data: {
-      success: true,
-      message: "Login Successful",
-      user,
-    },
-  };
+  return response;
 };
 
 export const logout = () => {
